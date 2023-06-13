0167e8ceaa8bc41d329117393f22e8b2b2bf786b
Fabricio Jonatan <martins.nascimento09@aluno.ifce.edu.br>
2023-06-12 21:00:31 -0300

    adsfasdf

--- testes/pika.py
+++
@@ -1,6 +0,0 @@
-num1 = input('Digite um número : ')
-num2 = input('Digite outro número : ')
-
-soma = num1 + num2
-
-print(f'O resultado da soma é {soma}')
---
+++ testes/repetição.py
@@ -0,0 +1,8 @@
+txt = 'rawom oytrz ckrfe uxtiv sqymt ghlgp v'
+inverso = []
+
+for letra in txt:
+    inverso.insert(0, letra)
+
+txtinverso = ''.join(inverso)
+print(txtinverso)
